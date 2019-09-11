235424ad1a6f5406d3a60a98118a550b7a90931c
synw <ansterj@gmail.com>
2019-09-11 09:17:02 +0200

    Improve the database schema

--- lib/src/database_schema.dart
+++ lib/src/database_schema.dart
@@ -1,8 +1,57 @@
+import 'package:flutter/material.dart';
 import 'package:sqlcool/sqlcool.dart';
 
+/// A list of all the default tables of the schema
+List<DbTable> geoPointTables = [
+  geoPointSchema,
+  geoSerieSchema,
+  geoPointPropertySchema,
+  geoPointImageSchema
+];
+
+/// The [GeoPoint] database sql schema
+final geoPointSchema = createGeoPointSchema();
+
+/// Tha [GeoSerie] schema
+final geoSerieSchema = createGeoSerieSchema();
+
+/// The [GeoPointProperty] schema
+final geoPointPropertySchema = createGeoPointPropertySchema();
+
+/// The geopoint image schema
+final geoPointImageSchema = createGeoPointImageSchema();
+
+/// A function to create a schema from table names
+List<DbTable> geoPointSqlSchema({
+  @required String geoPointTableName,
+  @required String geoSerieTableName,
+  String geoPointImageTableName,
+  String geoPointPropertyTableName,
+}) {
+  final tables = <DbTable>[];
+  final gp = createGeoPointSchema(
+      tableName: geoPointTableName, geoSerieTableName: geoSerieTableName);
+  final gs = createGeoSerieSchema(
+      tableName: geoSerieTableName, geoPointTableName: geoPointImageTableName);
+  tables.add(gp);
+  tables.add(gs);
+  if (geoPointImageTableName != null) {
+    tables.add(createGeoPointImageSchema(
+        tableName: geoPointImageTableName,
+        geoPointTableName: geoPointTableName));
+  }
+  if (geoPointPropertyTableName != null) {
+    tables.add(createGeoPointPropertySchema(
+        tableName: geoPointPropertyTableName,
+        geoPointTableName: geoPointTableName));
+  }
+  return tables;
+}
+
 /// Create a geopoint schema for a given table name
-DbTable createGeoPointSchema({String tableName = "geopoint"}) {
-  final table = DbTable(tableName)
+DbTable createGeoPointSchema(
+    {String tableName = "geopoint", String geoSerieTableName = "geoserie"}) {
+  return DbTable(tableName)
     ..varchar("name")
     ..varchar("slug", unique: true, nullable: true)
     ..timestamp()
@@ -21,35 +70,47 @@ DbTable createGeoPointSchema({String tableName = "geopoint"}) {
     ..varchar('region', nullable: true)
     ..varchar("subregion", nullable: true)
     ..varchar("country", nullable: true)
-    ..foreignKey("geoserie", nullable: true);
-  return table;
+    ..index("name")
+    ..foreignKey(geoSerieTableName, nullable: true, onDelete: OnDelete.cascade);
 }
 
-/// The geopoint database sql schema
-final geoPointSchema = createGeoPointSchema();
+/// Geopoint property schema generator
+DbTable createGeoPointPropertySchema(
+    {String tableName = "geopoint_property",
+    String geoPointTableName = "geopoint"}) {
+  return DbTable(tableName)
+    ..varchar("name")
+    ..varchar("value")
+    ..varchar("type")
+    ..foreignKey(geoPointTableName, onDelete: OnDelete.cascade)
+    ..index("name");
+}
 
-/*
-/// The geopoint property schema
-final geoPointPropertySchema = DbTable("geopoint_property")
-  ..varchar("name")
-  ..varchar("value", nullable: true)
-  ..varchar("type", check: 'type="int" or type="double" or type="string"')
-  ..foreignKey("geopoint", onDelete: OnDelete.cascade);*/
+/// The geoserie database schema generator
+DbTable createGeoSerieSchema(
+    {String tableName = "geoserie", String geoPointTableName = "geopoint"}) {
+  return DbTable(tableName)
+    ..varchar("name")
+    ..varchar("slug", unique: true, nullable: true)
+    ..varchar("type",
+        check: 'type = "group" or type = "line" or type="polygon"')
+    ..real("surface", nullable: true)
+    ..index("name")
+    ..foreignKey("centroid",
+        reference: geoPointTableName,
+        onDelete: OnDelete.cascade,
+        nullable: true)
+    ..foreignKey("boundary",
+        reference: tableName, onDelete: OnDelete.cascade, nullable: true);
+}
 
-/// The geoserie database schema
-final geoSerieSchema = DbTable("geoserie")
-  ..varchar("name")
-  ..varchar("slug", unique: true, nullable: true)
-  ..varchar("type", check: 'type = "group" or type = "line" or type="polygon"')
-  ..real("surface", nullable: true)
-  ..foreignKey("centroid",
-      reference: "geopoint", onDelete: OnDelete.cascade, nullable: true)
-  ..foreignKey("boudaries",
-      reference: "geoserie", onDelete: OnDelete.cascade, nullable: true);
-
-/// The geopoint image database schema
-final geoPointImageSchema = DbTable("geopoint_image")
-  ..varchar('path', nullable: true)
-  ..varchar("url",
-      nullable: true, check: "path IS NOT NULL OR (url IS NOT NULL)")
-  ..foreignKey("geopoint");
+/// The geopoint image database schema generator
+DbTable createGeoPointImageSchema(
+    {String tableName = "geopoint_image",
+    String geoPointTableName = "geopoint"}) {
+  return DbTable(tableName)
+    ..varchar('path', nullable: true)
+    ..varchar("url",
+        nullable: true, check: "path IS NOT NULL OR (url IS NOT NULL)")
+    ..foreignKey(geoPointTableName, onDelete: OnDelete.cascade);
+}
